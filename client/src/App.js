97d0f9b8a9e8b5b9be6bb82d3a1a0e1f7982e0f9
Lorenzo Bruno <brunolorenzo0911@gmail.com>
2023-08-08 13:01:22 +0200

    first commit , back and front sharing data

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import io from 'socket.io-client'
+import React, { useState } from 'react'
+import Chat from './Chat'
+
+const socket = io.connect("http://localhost:3001")
+
+function App() {
+
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    if(username !== "" && room !== ""){
+      socket.emit("join_room", room)
+    }
+  }
+
+
+  return (
+    <div className="App">
+      <h3>Join a chat !</h3>
+      <input type="text" placeholder="John" onChange={(event) =>{
+        setUsername(event.target.value)}}>
+      </input>
+      <input type="text" placeholder="Room 1" onChange={(event) =>{
+        setRoom(event.target.value)}}>
+      </input>
+      <button onClick={joinRoom}>Join a room</button>
+
+ 
+      <Chat socket={socket} username={username} room={room}/>
+    </div>
+
+  );
+}
+
+export default App;
